5f507793576ef40d0484a29981665b6a459b43da
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-05-28 20:01:13 +0700

    Menambahkan fungsi void remove

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -46,4 +46,15 @@ public:
         queue_array[REAR] = num;
     }
 
-    
+    void remove()
+    {
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        
