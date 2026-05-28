4429d7f056a100499b58ca109fbb2e310d43494b
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-05-28 20:03:43 +0700

    Menambahkan pengecekan queue kosong pada display

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -78,4 +78,13 @@ public:
         int FRONT_Position = FRONT;
         int REAR_Position = REAR;
 
+        // cek apakah antrian kosong
+        if (FRONT_Position == -1)
+        {
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        cout << "\nElements in the queue are....\n";
+
         
